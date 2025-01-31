cdb7bc40f2082545b81efd62acf96757f907d91b
hunterAntal <hunterantalgithub@gmail.com>
2025-01-30 21:35:22 -0500

    TTS Works

---
+++ src/yappers.py
@@ -0,0 +1,10 @@
+from yapper import Yapper, PiperSpeaker, PiperVoiceUK, PiperQuality
+
+alan = PiperSpeaker(
+    voice=PiperVoiceUK.ALAN
+)
+
+alan.say("Fuck you nigger! You stupid cunt?")
+#alan.say("expected AI driven utopia, got world domination instead")
+
+#alan.say("Danny is a small brained, longed toe having, small penis rocking, trailer trash baby! ")
